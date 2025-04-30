8f706e88b75a79e0cedbf48ac0422cd2308139d1
Minh Phuc <minhphuc3589@gmail.com>
2025-04-30 15:25:09 +0700

    Update: get_data() of file_handler.py

--- file_handler.py
+++ file_handler.py
@@ -1,12 +1,32 @@
 from tkinter import filedialog
 
+def save_file_name(path: str, data: str) -> None:
+    with open(path, "a+") as file:
+        file.write(str)
 
-def save_file_name(path: str, data: str):
-    with open("history/history.txt", "a+") as file:
-        file.write()
+def get_data(path: str, split_char: str = " ") -> [{}]:
 
-def get_data(path: str):
-    pass
+    opened_file_history = []
+
+    try:
+        with open(path, "r") as file:
+            text_line = file.read()
+            splitted_text_line = text_line.splt(split_char)
+
+            date_time = splitted_text_line[0]
+            dir_name = splitted_text_line[1]
+            file_name = splitted_text_line[2]
+
+            opened_file_history.append({
+                "date_time": date_time,
+                "dir_name": dir_name,
+                "file_name": file_name
+            })
+
+        return opened_file_history
+
+    except:
+        print("get_data err")
 
 def get_file_name() -> str:
     return filedialog.askopenfilename(
